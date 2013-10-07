95592e95cd68a6ca1f4a2726143afd9c066ed84d
Cédric Salaun <cedric.salaun@gmail.com>
2013-10-07 15:10:32 +0200

    Update views.py
    replace tab with space. delete unuseful tab.

--- Django/app/views.py
+++ Django/app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts 		import render_to_response
 from django.template 		import Context, Template, RequestContext
-from django.http 			import HttpResponseRedirect, HttpResponse
+from django.http 		    import HttpResponseRedirect, HttpResponse
 from django.contrib.auth 	import authenticate, login
 
-from django.core.paginator 				import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator 		        import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators 	import login_required
-from datetime 							import datetime,timedelta
+from datetime 				            import datetime,timedelta
 
 from app.models 	import *
 
@@ -26,23 +26,23 @@ def get_posts(posts, page):
 
 def index(request):
 	""" index page """
-	posts 	= Post.objects.all()
-	page 	= request.GET.get('page')
+	posts = Post.objects.all()
+	page = request.GET.get('page')
 
 	data = {}
-	data['articles'] 		= get_posts(posts, page)
-	data['categories'] 		= Categorie.objects.all()
+	data['articles'] = get_posts(posts, page)
+	data['categories'] = Categorie.objects.all()
 	return render_to_response('index.html', data , context_instance=RequestContext(request))
 
 def article(request, slug):
 	""" single article view """
-	data 		= {}
-	post 		= Post.objects.filter(slug = slug)
+	data = {}
+	post = Post.objects.filter(slug = slug)
 	if post.count() > 0 :
 		post = post[0]
-		data['post'] 			= post
-		data['comments']    	= post.comments.all()
-		data['categories'] 		= Categorie.objects.all()
+		data['post'] = post
+		data['comments'] = post.comments.all()
+		data['categories'] = Categorie.objects.all()
 		
 		return render_to_response('post.html', data , context_instance=RequestContext(request))
 	else :
@@ -50,13 +50,13 @@ def article(request, slug):
 
 def comment(request, p_id) :
 	""" comment a post """
-	data 				= {}
-	post 				= Post.objects.filter(id = p_id)
+	data = {}
+	post = Post.objects.filter(id = p_id)
 	if post.count() > 0 :
 		post = post[0]
-		mail 			= request.POST.get('email', '')
-		username 		= request.POST.get('username', '')
-		content 		= request.POST.get('content', '')
+		mail = request.POST.get('email', '')
+		username = request.POST.get('username', '')
+		content = request.POST.get('content', '')
 
 		if len(mail) > 0 and len(username) > 0 :
 			comment = Comment(mail = mail, username=username, content=content, created = datetime.now(), post = post )
@@ -72,23 +72,23 @@ def comment(request, p_id) :
 
 def category(request, slug):
 	""" category post """
-	categorie 	= Categorie.objects.get(slug = slug)
-	posts 	  	= Post.objects.filter(categorie = categorie)
-	page 		= request.GET.get('page')
+	categorie = Categorie.objects.get(slug = slug)
+	posts = Post.objects.filter(categorie = categorie)
+	page = request.GET.get('page')
 
 	data = {}
-	data['articles'] 		= get_posts(posts, page)
-	data['categories'] 		= Categorie.objects.all()
+	data['articles'] = get_posts(posts, page)
+	data['categories'] = Categorie.objects.all()
 	return render_to_response('index.html', data , context_instance=RequestContext(request))
 
 def author(request, user_id):
 	""" author post """
-	posts 	  	= Post.objects.filter(user__id = user_id)
-	page 		= request.GET.get('page')
+	posts = Post.objects.filter(user__id = user_id)
+	page = request.GET.get('page')
 
 	data = {}
-	data['articles'] 		= get_posts(posts, page)
-	data['categories'] 		= Categorie.objects.all()
+	data['articles'] = get_posts(posts, page)
+	data['categories'] = Categorie.objects.all()
 	return render_to_response('index.html', data , context_instance=RequestContext(request))
 
 def loginView(request):
@@ -112,30 +112,30 @@ def loginView(request):
 @login_required
 def adminIndex(request):
 	""" admin index view """
-	data 	= {}
-	posts 	= Post.objects.all()
-	page 	= request.GET.get('page')
+	data = {}
+	posts = Post.objects.all()
+	page = request.GET.get('page')
 
 	data = {}
-	data['admin']			= True
-	data['articles'] 		= get_posts(posts, page)
+	data['admin'] = True
+	data['articles'] = get_posts(posts, page)
 	return render_to_response('admin/index.html' , data , context_instance=RequestContext(request))
 
 @login_required
 def adminEdit(request, post_id = 0):
 	if request.POST :
 		""" post data, we update the post """
-		post_id     		= request.POST.get('postID', '')
+		post_id = request.POST.get('postID', '')
 		
-		cat_id 				= request.POST.get('PostCategoryId', '')
-		usr_id 				= request.POST.get('PostUserId', '')
+		cat_id = request.POST.get('PostCategoryId', '')
+		usr_id = request.POST.get('PostUserId', '')
 
-		post 				= Post.objects.get(id = post_id)
-		post.name   		= request.POST.get('PostName', '')
-		post.slug 			= request.POST.get('PostSlug', '')
-		post.user   		= User.objects.get(id = usr_id)
-		post.categorie 		= Categorie.objects.get(id = cat_id)
-		post.content 		= request.POST.get('PostContent', '')
+		post = Post.objects.get(id = post_id)
+		post.name = request.POST.get('PostName', '')
+		post.slug = request.POST.get('PostSlug', '')
+		post.user = User.objects.get(id = usr_id)
+		post.categorie = Categorie.objects.get(id = cat_id)
+		post.content = request.POST.get('PostContent', '')
 		post.save()
 
 		return HttpResponseRedirect("/admin/index")
@@ -145,26 +145,26 @@ def adminEdit(request, post_id = 0):
 		data = {}
 		post = Post.objects.get(id = post_id)
 
-		data['categories'] 		= Categorie.objects.all()
-		data['users'] 			= User.objects.all()
-		data['admin']			= True
-		data['post'] 			= post
+		data['categories'] = Categorie.objects.all()
+		data['users'] = User.objects.all()
+		data['admin'] = True
+		data['post'] = post
 		return render_to_response('admin/edit.html' , data , context_instance=RequestContext(request))
 
 @login_required
 def adminNew(request, post_id = 0):
 	if request.POST :
 		""" post data, we add the post """	
-		cat_id 				= request.POST.get('PostCategoryId', '')
-		usr_id 				= request.POST.get('PostUserId'	   , '')
-		name   				= request.POST.get('PostName'      , '')
-		slug 				= request.POST.get('PostSlug'      , '')
-		content 			= request.POST.get('PostContent'   , '')
+		cat_id 	= request.POST.get('PostCategoryId', '')
+		usr_id 	= request.POST.get('PostUserId'	   , '')
+		name = request.POST.get('PostName'      , '')
+		slug = request.POST.get('PostSlug'      , '')
+		content = request.POST.get('PostContent'   , '')
 
-		user   				= User.objects.get(id = usr_id)
-		categorie 			= Categorie.objects.get(id = cat_id)
+		user = User.objects.get(id = usr_id)
+		categorie = Categorie.objects.get(id = cat_id)
 
-		post 				= Post(name = name, slug = slug, user = user, categorie = categorie, content = content)
+		post = Post(name = name, slug = slug, user = user, categorie = categorie, content = content)
 		post.save()
 
 		return HttpResponseRedirect("/admin/index")
@@ -173,16 +173,16 @@ def adminNew(request, post_id = 0):
 		""" no post data, this is simple view access """
 		data = {}
 
-		data['categories'] 		= Categorie.objects.all()
-		data['users'] 			= User.objects.all()
-		data['admin']			= True
+		data['categories'] = Categorie.objects.all()
+		data['users'] = User.objects.all()
+		data['admin'] = True
 		return render_to_response('admin/edit.html' , data , context_instance=RequestContext(request))
 
 @login_required
 def adminDelete(request, post_id = 0):
 	""" delete a post """
 	if post_id > 0 :
-		post 		= Post.objects.filter(id = post_id)
+		post = Post.objects.filter(id = post_id)
 		if post.count() > 0 :
 			post = post[0]
 			post.delete()
